8a5aea8e02522573bb39acc1d56d040bfc6068bb
Ezek1eru <ezelf.el24@gmail.com>
2022-10-06 17:01:43 -0300

    Rutas

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+export default function Card(props) {
+  return (
+    <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+      <div className="flex h-12 w-12 items-center justify-center rounded-full border border-gray-100 bg-gray-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          />
+        </svg>
+      </div>
+      <div className="ml-4">
+        <h2 className="font-semibold">{props.children}</h2>
+        <p className="mt-2 text-sm text-gray-500">{props.apellidos}</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,9 +1,17 @@
 import DefaultLayout from "../components/DefaultLayout";
+import Card from "../components/Card";
 
 export default function Usuarios() {
   return (
     <DefaultLayout>
-      <h1>Usuarios</h1>
+      <div className=" m-4 grid grid-cols-4 gap-4">
+        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+      </div>
     </DefaultLayout>
   );
 }
