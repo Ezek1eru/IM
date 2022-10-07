29a4e534567085ab1a8eff3f165c23b7a46c595a
Ezek1eru <ezelf.el24@gmail.com>
2022-10-07 09:28:59 -0300

    Directiva page

--- src/pages/Directiva.jsx
+++ src/pages/Directiva.jsx
@@ -1,9 +1,51 @@
 import DefaultLayout from "../components/DefaultLayout";
+import Card from "../components/Card";
 
 export default function Directiva() {
   return (
     <DefaultLayout>
-      <h1>Directiva</h1>
+      <div className=" m-4 grid grid-cols-4 gap-4">
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500"> email@uap.edu.ar </p>
+          <p className="mt-3 text-sm text-gray-500">Grupo</p>
+        </Card>
+      </div>
     </DefaultLayout>
   );
 }
