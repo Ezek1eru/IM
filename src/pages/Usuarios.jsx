930055bfa52a395056fb310305607cbe3a112706
Ezek1eru <ezelf.el24@gmail.com>
2022-10-07 08:20:40 -0300

    Página Usuers

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -5,12 +5,22 @@ export default function Usuarios() {
   return (
     <DefaultLayout>
       <div className=" m-4 grid grid-cols-4 gap-4">
-        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
-        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
-        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
-        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
-        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
-        <Card children="Nombre" apellidos="email@uap.edu.ar"></Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500">email@uap.edu.ar</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500">email@uap.edu.ar</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500">email@uap.edu.ar</p>
+        </Card>
+        <Card>
+          <h2 className="font-semibold">Nombre</h2>
+          <p className="mt-2 text-sm text-gray-500">email@uap.edu.ar</p>
+        </Card>
       </div>
     </DefaultLayout>
   );
