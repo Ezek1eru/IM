f49a340b1a2fe8d71e07fe6122692f7da523ff8c
Ezek1eru <ezelf.el24@gmail.com>
2022-10-11 09:10:30 -0300

    Form

--- src/components/AddEditForm.jsx
+++ src/components/AddEditForm.jsx
@@ -1,11 +1,36 @@
 export default function AddEdditForm() {
-    
-
   return (
-    <div className="flex items-center justify-center p-11">
+    <div
+      className="flex items-center justify-center p-7"
+      id="authentication-modal"
+      tabindex="-1"
+      aria-hidden="true"
+    >
       <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-100 shadow-md">
         <div className="mx-auto w-full max-w-[550px]">
-          <form action="https://formbold.com/s/FORM_ID" method="POST">
+          <form>
+            <div className="flex ">
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                data-modal-toggle="authentication-modal"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
             <div className="-mx-3 flex flex-wrap">
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
@@ -41,6 +66,7 @@ export default function AddEdditForm() {
                   />
                 </div>
               </div>
+              <span className="sr-only">Close modal</span>
             </div>
             <div className="mb-5">
               <label
@@ -84,7 +110,7 @@ export default function AddEdditForm() {
                 id="carrera"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               >
-                <option selected>Categoría</option>
+                <option>Categoría</option>
                 <option value="readonly">Económicas</option>
                 <option value="readonly">Humanidades</option>
                 <option value="readonly">Salud</option>
@@ -106,7 +132,7 @@ export default function AddEdditForm() {
                 id="carrera"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               >
-                <option selected>Carrera</option>
+                <option>Carrera</option>
                 <option value="readonly">Económicas</option>
                 <option value="readonly">Humanidades</option>
                 <option value="readonly">Salud</option>
