dcf2cc09528d6778d445aaf3f9309c2eefd0fb55
BrauerSebastian <sebastianreppvale2@gmail.com>
2022-11-01 06:15:19 -0300

    Login v1

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import Directiva from "./pages/Directiva";
 import Cargos from "./pages/Cargos";
 import Salidas from "./pages/Salidas";
 import Usuarios from "./pages/Usuarios";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/misioneros" element={<Misioneros />}></Route>
-          <Route path="/grupos" element={<Grupos />}></Route>
-          <Route path="/directiva" element={<Directiva />}></Route>
-          <Route path="/usuarios" element={<Salidas />}></Route>
-          <Route path="/salidas" element={<Usuarios />}></Route>
-          <Route path="/cargos" element={<Cargos />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/misioneros" element={<Misioneros />}></Route>
+            <Route path="/grupos" element={<Grupos />}></Route>
+            <Route path="/directiva" element={<Directiva />}></Route>
+            <Route path="/usuarios" element={<Salidas />}></Route>
+            <Route path="/salidas" element={<Usuarios />}></Route>
+            <Route path="/cargos" element={<Cargos />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,86 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { Alert } from "./Alert";
+
 export default function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { login} = useAuth();
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await login(user.email, user.password);
+      navigate("/");
+    } catch (error) {
+      setError("El usuario o contraseña es inválido, prube de nuevo");
+    }
+  };
+
+  const handleChange = ({ target: { value, name } }) =>
+    setUser({ ...user, [name]: value });
+
   return (
     <div className="flex flex-row h-screen w-screen items-center justify-around bg-gradient-to-r from-red-700 to-red-500">
-      <div className="h-96">
-        <lottie-player
-          src="https://assets2.lottiefiles.com/packages/lf20_z2sbj8cm.json"
-          background="transparent"
-          speed="1"
-          autoplay
-        ></lottie-player>
-      </div>
-      <div className="container max-w-md shadow-md hover:shadow-lg transition duration-300">
-        <div className="py-12 p-10 bg-white rounded-xl">
-          <div className="mb-6">
-            <label
-              className="mr-4 text-gray-700 font-bold inline-block mb-2"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-              placeholder="Your name"
-            />
-          </div>
-          <div>
-            <label
-              className="mr-4 text-gray-700 font-bold inline-block mb-2"
-              htmlFor="name"
-            >
-              Email
-            </label>
-            <input
-              type="text"
-              className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-              placeholder="@email"
-            />
-          </div>
-          <button className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300">
-            LOGIN
-          </button>
+      {error && <Alert message={error} />}
+      <form onSubmit={handleSubmit}>
+        <div className="h-96">
+          <lottie-player
+            src="https://assets2.lottiefiles.com/packages/lf20_z2sbj8cm.json"
+            background="transparent"
+            speed="1"
+            autoplay
+          ></lottie-player>
         </div>
-      </div>
+        <div className="container max-w-md shadow-md hover:shadow-lg transition duration-300">
+          <div className="py-12 p-10 bg-white rounded-xl">
+            <div className="mb-6">
+              <label
+                className="mr-4 text-gray-700 font-bold inline-block mb-2"
+                htmlFor="name"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                onChange={handleChange}
+                name="email"
+                id="email"
+                className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                placeholder="@email"
+              />
+            </div>
+            <div>
+              <label
+                className="mr-4 text-gray-700 font-bold inline-block mb-2"
+                htmlFor="name"
+              >
+                Contraseña
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={handleChange}
+                className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                placeholder="*************"
+              />
+            </div>
+            <button 
+              type="submit" 
+              className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300"
+            >
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
