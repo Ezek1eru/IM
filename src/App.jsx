e412636adb896de845bd2639f820d7cd8856141f
Ezek1eru <ezelf.el24@gmail.com>
2022-11-22 09:39:39 -0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dropdown from "./components/Dropdown";
+import { AuthProvider } from "./context/AuthContext";
 import Cargos from "./pages/Cargos";
 import Directiva from "./pages/Directiva";
 import Grupos from "./pages/Grupos";
@@ -7,13 +8,8 @@ import GrupoUserPage from "./pages/GrupoUserPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Misioneros from "./pages/Misioneros";
-import Grupos from "./pages/Grupos";
-import Directiva from "./pages/Directiva";
-import Cargos from "./pages/Cargos";
 import Salidas from "./pages/Salidas";
 import Usuarios from "./pages/Usuarios";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
@@ -26,8 +22,8 @@ function App() {
             <Route path="/misioneros" element={<Misioneros />}></Route>
             <Route path="/grupos" element={<Grupos />}></Route>
             <Route path="/directiva" element={<Directiva />}></Route>
-            <Route path="/usuarios" element={<Salidas />}></Route>
-            <Route path="/salidas" element={<Usuarios />}></Route>
+            <Route path="/usuarios" element={<Usuarios />}></Route>
+            <Route path="/salidas" element={<Salidas />}></Route>
             <Route path="/cargos" element={<Cargos />}></Route>
           </Routes>
         </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
     email: "",
     password: "",
   });
-  const { login} = useAuth();
+  const { login } = useAuth();
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -27,98 +27,59 @@ export default function Login() {
     setUser({ ...user, [name]: value });
 
   return (
-    <div className="flex flex-row h-screen w-screen items-center justify-around bg-gradient-to-r from-red-700 to-red-500">
+    <div>
       {error && <Alert message={error} />}
       <form onSubmit={handleSubmit}>
-        <div className="h-96">
-          <lottie-player
-            src="https://assets2.lottiefiles.com/packages/lf20_z2sbj8cm.json"
-            background="transparent"
-            speed="1"
-            autoplay
-          ></lottie-player>
-        </div>
-        <div className="container max-w-md shadow-md hover:shadow-lg transition duration-300">
-          <div className="py-12 p-10 bg-white rounded-xl">
-            <div className="mb-6">
-              <label
-                className="mr-4 text-gray-700 font-bold inline-block mb-2"
-                htmlFor="name"
+        <div className="flex flex-row h-screen w-screen items-center justify-around bg-gradient-to-r from-red-700 to-red-500">
+          <div className="h-96">
+            <lottie-player
+              src="https://assets2.lottiefiles.com/packages/lf20_z2sbj8cm.json"
+              background="transparent"
+              speed="1"
+              autoplay
+            ></lottie-player>
+          </div>
+          <div className="container max-w-md shadow-md hover:shadow-lg transition duration-300">
+            <div className="py-12 p-10 bg-white rounded-xl">
+              <div className="mb-6">
+                <label
+                  className="mr-4 text-gray-700 font-bold inline-block mb-2"
+                  htmlFor="name"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  name="email"
+                  id="email"
+                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                  placeholder="@email"
+                />
+              </div>
+              <div>
+                <label
+                  className="mr-4 text-gray-700 font-bold inline-block mb-2"
+                  htmlFor="name"
+                >
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={handleChange}
+                  className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                  placeholder="*************"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300"
               >
-                Email
-              </label>
-              <input
-                type="text"
-                onChange={handleChange}
-                name="email"
-                id="email"
-                className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                placeholder="@email"
-              />
+                Iniciar Sesión
+              </button>
             </div>
-            <div>
-              <label
-                className="mr-4 text-gray-700 font-bold inline-block mb-2"
-                htmlFor="name"
-              >
-                Contraseña
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                onChange={handleChange}
-                className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                placeholder="*************"
-              />
-            </div>
-            <button 
-              type="submit" 
-              className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300"
-            >
-=======
-
-export default function Login() {
-
-  
-  return (
-    <div className="flex flex-row h-screen w-screen items-center justify-around bg-gradient-to-r from-red-700 to-red-500">
-      <div className="h-96">
-        <lottie-player
-          src="https://assets2.lottiefiles.com/packages/lf20_z2sbj8cm.json"
-          background="transparent"
-          speed="1"
-          autoplay
-        ></lottie-player>
-      </div>
-      <form action="/login" method="post">
-        <div className="container max-w-md shadow-md hover:shadow-lg transition duration-300">
-          <div className="py-12 p-10 bg-white rounded-xl">
-            <div className="mb-6">
-              <label className="mr-4 text-gray-700 font-bold inline-block mb-2">
-                Nombre de usuario
-              </label>
-              <input
-                type="text"
-                className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                placeholder="@email"
-                name="nombre_usuario"
-              />
-            </div>
-            <div>
-              <label className="mr-4 text-gray-700 font-bold inline-block mb-2">
-                Contraseña
-              </label>
-              <input
-                type="text"
-                className="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
-                placeholder="Password"
-                name="password"
-              />
-            </div>
-            <button className="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300">
-              LOGIN
-            </button>
           </div>
         </div>
       </form>
